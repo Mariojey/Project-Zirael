8a6bcf9cdfee74ce0de3c084e8e18ed9f68e6f18
Mariusz Jacek <marios.yaceg@gmail.com>
2022-10-09 15:22:47 +0200

    Fix Ract Navigation Native

--- frontend/App.js
+++ frontend/App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Lottie from 'lottie-react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function Splash(){
   return (
     <View style={styles.container}>
       <Lottie source={require('./assets/splash.json')} autoPlay loop />
@@ -11,12 +13,31 @@ export default function App() {
   );
 }
 
+function HomeScreen(){
+  <View style={styles.container}>
+      <Text style={styles.containerText}>Hello Citizen!</Text>
+  </View>
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Splash" component={Splash} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
+    display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
   },
   containerText: {
     color: '#fff',
