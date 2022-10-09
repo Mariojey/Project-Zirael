a290360ed3ccb22ba0c7b88f0790de19f0a62de5
Mariusz Jacek <marios.yaceg@gmail.com>
2022-10-09 13:48:30 +0200

    Add VoteSplash

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Lottie from 'lottie-react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Lottie source={require('./assets/splash.json')} autoPlay loop />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerText: {
+    color: '#fff',
+    fontSize: 30,
+  }
 });
