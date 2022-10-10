224979e400dd309a567db739ee39b6f7a6c59f03
Zongi <67127030+zongii@users.noreply.github.com>
2022-10-10 18:23:46 +0200

    Update region.js

--- backend/routes/region.js
+++ backend/routes/region.js
@@ -4,8 +4,8 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/conn');
 
-router.get('/find/:name', (req, res) => {
-    res.send(req.params.name);
+router.get('/find/:name', async (req, res) => {
+    res.send("test");
 })
 
 module.exports = router
