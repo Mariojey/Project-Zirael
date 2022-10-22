1ff351e82529da0f0bef42ce7e3beb20dca9b2c1
Mariusz Jacek <marios.yaceg@gmail.com>
2022-10-22 13:34:52 +0200

    webbuilt

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, } from 'react-native';
 
 
+
 function HomeScreen(){
     return(
         <View style={styles.container}>
